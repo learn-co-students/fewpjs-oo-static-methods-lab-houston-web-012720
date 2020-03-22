ee8c9f055ac11c576f30873d0428c56a8e3bdc24
vienpham2019 <vienpham2019@gmail.com>
2020-03-22 13:16:47 -0500

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
 class Formatter {
   //add static methods here
+  static capitalize(string) {
+    return string.split(" ").map(w => w = w.charAt(0).toUpperCase() + w.slice(1)).join(" ")
+  } 
+
+  static sanitize(string){
+    return string.replace(/[^A-Za-z0-9-" "']/g,'')
+  }
+
+  static titleize(string){
+    let arr = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by','from']
+    let string_arr = string.split(" ")
+    return string_arr.map(w => 
+      !arr.includes(w) || string_arr.indexOf(w) === 0? this.capitalize(w) : w
+    ).join(" ")
+  }
 }
